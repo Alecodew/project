ef39727e6888cccc0a32fcbc16238550e5cca5f0
alecodew <alexey.bondarev93.gmail.com>
2021-04-26 19:43:02 +0700

    26.04.2021

---
+++ udemy/study/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let numder=4.5;
+
+console.log(4/0);
+console.log('string'*9);
+
+const persone="alex";
+
+const bool= true;
+
+const obj = {
+    name: 'john',
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+
+
+let arr= ['plum.png','orange.jpg',6,'apple.bmp',{},[]];
+
+console.log(arr[1]);
+/* 
+const result= confirm("are you here?");
+console.log(result); */
+
+
+/* const answer = +prompt ("Вам есть 18?","18");
+console.log(answer + 5); */
+
+/* const answers = [];
+
+/* answers[0] = prompt('как ваше имя?','');
+answers[1] = prompt('как ваша фамилия?','');
+answers[2] = prompt('сколько вам лет?',''); */
+
+/* console.log(typeof(answers)); */ 
+
+console.log('arr' + "- object");
+
+let incr = 10,
+    decr = 10;
+
+/* ++incr;
+--decr; */
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
