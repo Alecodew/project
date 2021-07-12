984377131d56cf45cffdd42ef1c6ee0781261243
alecodew <alexey.bondarev93.gmail.com>
2021-07-12 21:07:31 +0700

    12331

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const logg = "hello world";
+console.log(logg.substr(2, 9));
+
+const test ="12.2px";
+console.log(parseInt(test));
--- udemy/study/script.js
+++ udemy/study/script.js
@@ -1,21 +1,3 @@
 "use strict";
 
-const numderOfFilms = +prompt ('Сколько фильмов вы уже посмотрели','');
 
-const personalMovieDB = {
-    count: numderOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt ('Один из последний просмотренных фильмов?',''),
-      b = prompt ('На сколько оцените его?',''),
-      c = prompt ('Один из последний просмотренных фильмов?',''),
-      d = prompt ('На сколько оцените его?','');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(numderOfFilms);
