e380151cdd87bf3892a6ef8e6b4156e3905b9c26
alecodew <alexey.bondarev93.gmail.com>
2021-07-13 22:35:58 +0700

    js studi

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-"use strict";
+'use strict';
 
-const logg = "hello world";
-console.log(logg.substr(2, 9));
+/* alert("I am javascript");
+alert('you'); */
 
-const test ="12.2px";
-console.log(parseInt(test));
+/* let message = 'hello world';
+
+alert(message); */
+
+let $kat = 'byba';
+
+let _sosya;
+
+_sosya = $kat
+alert(_sosya);
+
+
