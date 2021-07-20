38b85db9688c6e1d52627c582a81d80e491c5ab4
alecodew <alexey.bondarev93.gmail.com>
2021-07-20 21:12:31 +0700

    JS

--- js/script.js
+++ js/script.js
@@ -58,9 +58,45 @@ if (login == 'Админ') {
      poo++;
  } */
 
- for (let i = 1; i<10; i++) {
+ /* for (let i = 1; i<10; i++) {
      if (i=== 6) {
          continue;
      }
 alert(i);
+ } */
+
+ const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+ for (let i=0; i<2; i++) {
+    const a = prompt ('Один из последних просмотренных фильмов?',''),
+          b = prompt ('На сколько оцените его?','');
+
+    if(a != null && b !=null && a !='' && b !='' && a.length<50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
  }
+
+ if(personalMovieDB.count<10){
+     console.log('Просмотрено довольно мало фильмов');
+ } else if(personalMovieDB.count>=10 && personalMovieDB<30) {
+    console.log('Вы классический зритель');
+ } else if(personalMovieDB.count>=30) {
+     console.log('Вы киноман');
+ }else {
+     console.log('Произошла ошибка');
+ }
+
+console.log(personalMovieDB);
